39310d66a32a27c23327ef846d353a62918dbc83
Achim Fritz <achim.fritz@b13.de>
2022-12-12 11:08:19 +0100

    [TASK] add TYPO3 v12 Support

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,12 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
     'state' => 'stable',
-    'uploadfolder' => false,
     'clearCacheOnLoad' => true,
-    'version' => '1.1.0',
+    'version' => '1.2.0',
     'constraints' =>
         [
-            'depends' => ['typo3' => '10.4.0-11.4.99'],
+            'depends' => ['typo3' => '10.4.0-12.99.99'],
             'conflicts' => [],
             'suggests' => [],
         ],
