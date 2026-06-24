8070b40d8f742bdffcb60d90b82afb91421ca1ab
Achim Fritz <af@achimfritz.de>
2026-06-24 11:09:07 +0200

    [RELEASE] 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'b13 GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.3.0',
+    'version' => '2.0.0',
     'constraints' =>
         [
             'depends' => ['typo3' => '13.4.0-14.99.99'],
